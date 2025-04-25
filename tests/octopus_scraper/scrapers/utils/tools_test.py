b98139233e11304bd93f7727a74e2e9582fdff14
Duan-JM <vincent.duan95@gmail.com>
2025-04-25 17:16:04 +0800

    add test & fix config token invalid

--- tests/octopus_scraper/scrapers/utils/tools_test.py
+++ tests/octopus_scraper/scrapers/utils/tools_test.py
@@ -6,14 +6,30 @@ from octopus_scraper.scrapers.utils.tools import (
     convert_contents_to_mk,
 )
 
+HTML_CONTENT = """
+<h1>欢迎来到我的网页</h1>
+<p>这是一个简单的HTML段落，用来展示如何使用HTML标签来编写内容。你可以在这里添加任意文本信息。</p>
+<p>想了解更多信息，请访问 
+<a href="https://www.example.com" target="_blank">我的主页</a>。
+</p>
+
+
+
+
+
+  <p>3个换行的测试</p>
+*  星号处理测试
+"""
+
 
 def test_convert_contents_to_mk():
     html_content = [
         {"value": "<p>This is <strong>bold</strong> text.</p>"},
         {"value": "<p>And <em>italic</em> text.</p>"},
+        {"value": HTML_CONTENT},
     ]
     markdown = convert_contents_to_mk(html_content)
-    expected = "This is __bold__ text.\nAnd _italic_ text.\n"
+    expected = "This is __bold__ text.\n\nAnd _italic_ text.\n\n# 欢迎来到我的网页\n\n这是一个简单的HTML段落，用来展示如何使用HTML标签来编写内容。你可以在这里添加任意文本信息。\n\n想了解更多信息，请访问 [我的主页](https://www.example.com)。\n\n3个换行的测试\n\n* 星号处理测试"
     assert markdown.strip() == expected.strip()
 
 
